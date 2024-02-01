08e718b333e4fd5d058a4f810a04ec1a868407e8
Timewound <rubenjms1@gmail.com>
2024-02-01 09:39:55 -0300

    mudando o banco de dados para mysql e incluindo novos models

--- nevil/settings.py
+++ nevil/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'nevil.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': getenv('ENGINE'),
+        'NAME': getenv('NAME'),
+        'HOST': getenv('HOST'),
+        'PORT': getenv('PORT'),
+        'USER': getenv('USER'),
+        'PASSWORD': getenv('PASSWORD'),
     }
 }
 
--- orçamentos/models.py
+++ orçamentos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    service_name = models.CharField(max_length=200)
+    modify_date = models.DateTimeField("date published")
+    oservation = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.service_name
+    
+class PriceRange(models.Model):
+    small_int = models.IntegerField()
+    large_int = models.IntegerField()    
+    price = models.FloatField()
